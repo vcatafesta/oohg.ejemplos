faaeb26e8b4459f3b477249e69231ccd97f71344
Fernando Yurisich <fyurisich@oohg.org>
2018-01-12 20:10:21 -0300

    2018-01-12 20:10 UTC-0300 Fernando Yurisich <fyurisich@oohg.org>
       * ejemplos\ChangeLog.txt
         * Updated.
       * ejemplos\bostaurus\e013.bat
       * ejemplos\zebra\e001.bat
         * Use HG_ROOT instead of hardwired path.

--- zebra/e001.bat
+++ zebra/e001.bat
@@ -1,9 +1,11 @@
 @echo off
 
+if "%HG_ROOT%"="" set HG_ROOT=c:\oohg
+
 Rem Si quiere utilizar hbmk2 para construir el exe, quite el comentario de la siguiente línea y comente las restantes:
-Rem BUILDAPP e001 -ic:\oohg\hb32\contrib\hbzebra
+Rem BuildApp e001 -i%HG_ROOT%\hb32\contrib\hbzebra
 
 Rem Si quiere utilizar harbour y gcc para construir el exe, quite el comentario de la siguiente línea y comente las restantes:
-Rem COMPILE e001 -ic:\oohg\hb32\contrib\hbzebra
+Rem COMPILE e001 -i%HG_ROOT%\hb32\contrib\hbzebra
 
-COMPILE e001 -ic:\oohg\hb32\contrib\hbzebra
+COMPILE e001 -i%HG_ROOT%\hb32\contrib\hbzebra
